ba33847129472ce850427994453e2399e6f71f6e
JidNation <jidnationdev@gmail.com>
2026-03-27 14:35:05 +0100

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by JidNation on 27/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
